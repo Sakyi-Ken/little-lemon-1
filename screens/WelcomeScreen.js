244c6274164fc138aaac928871c7a3daec36acd8
Sakyi-Ken <esappiah009@st.ug.edu.gh>
2024-09-27 05:50:24 +0000

    make welcome screen scrollable

--- screens/WelcomeScreen.js
+++ screens/WelcomeScreen.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import { View, Text} from 'react-native';
+import { ScrollView, Text} from 'react-native';
 
 export default function WelcomeScreen () {
   return (
-  <View style={{ flex: 1}}>
+  <ScrollView style={{ flex: 1}}>
     <Text style={{
       padding: 40,
       fontSize: 30,
@@ -14,13 +14,13 @@ export default function WelcomeScreen () {
     </Text>
     <Text style={{
       padding: 20,
-      fontSize: 24,
+      fontSize: 46,
       marginVertical: 8,
       color: '#EDEFEE',
       textAlign: 'center',
     }}>
       Little Lemon is a charming neighborhood bistro that serves simple food and classic cocktails in a lively but casual environment. We would love to hear more about your experience with us!
     </Text>
-  </View>
+  </ScrollView>
   )
 }
