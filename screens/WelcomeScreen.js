bbab7f64d3bd9ba1748e106eb49cbf93fc4eead4
Sakyi-Ken <esappiah009@st.ug.edu.gh>
2024-09-30 02:48:13 +0000

    Add a text input to the welcome page.

--- screens/WelcomeScreen.js
+++ screens/WelcomeScreen.js
@@ -1,7 +1,9 @@
-import React from 'react';
-import { ScrollView, Text, StyleSheet} from 'react-native';
+import React, { useState } from 'react';
+import { ScrollView, Text, StyleSheet, TextInput } from 'react-native';
 
 export default function WelcomeScreen () {
+  const [firstName, onChangeFirstName] = useState('');
+
   return (
   <ScrollView style={welcomeStyles.container}>
     <Text style={welcomeStyles.headerText}>
@@ -10,6 +12,12 @@ export default function WelcomeScreen () {
     <Text style={welcomeStyles.regularText}>
       Little Lemon is a charming neighborhood bistro that serves simple food and classic cocktails in a lively but casual environment. We would love to hear more about your experience with us!
     </Text>
+    <TextInput 
+      value={firstName}
+      onChangeText={onChangeFirstName}
+      style={welcomeStyles.input}
+      placeholder="First Name"  
+    />
   </ScrollView>
   )
 }
@@ -31,4 +39,13 @@ const welcomeStyles = StyleSheet.create({
     color: '#EDEFEE',
     textAlign: 'center',
   },
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+    fontSize: 16,
+    borderColor: '#EDEFEE',
+    backgroundColor: '#EDEFEE',
+  }
 });
