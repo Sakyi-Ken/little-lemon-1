b2735ffcf5014fbce8f07127cf3f5966bcbaef1d
Sakyi-Ken <esappiah009@st.ug.edu.gh>
2024-10-02 14:08:42 +0000

    Add images to Welcome.js

--- screens/Welcome.js
+++ screens/Welcome.js
@@ -1,14 +1,48 @@
 import React from 'react';
-import {View, Image, StyleSheet, Text,} from 'react-native';
+import {View, Image, StyleSheet, Text, ScrollView} from 'react-native';
 
 const Welcome = () => {
   return (
-    <View style={welcomeStyles.container}>
-      <Image style={welcomeStyles.logo} source={require('../img/Little-Lemon-Logo.png')} />
+    <ScrollView style={welcomeStyles.container}>
+      <Image 
+        style={welcomeStyles.logo} 
+        source={require('../img/Little-Lemon-Logo.png')}
+        resizeMode="center" 
+        accessible={true}
+        accessibilityLabel={'Little Lemon Logo'}
+      />
       <Text style={welcomeStyles.title}>
         Little Lemon, your local Mediterranean Bistro
       </Text>
-    </View>
+      <Image
+        style={welcomeStyles.image} 
+        source={require('../img/Picture1.png')}
+        resizeMode='cover'
+        accessibility={true}
+        accessibilityLabel={'A menu list on top of a table'}
+      />
+      <Image
+        style={welcomeStyles.image} 
+        source={require('../img/Picture2.png')}
+        resizeMode='cover'
+        accessibility={true}
+        accessibilityLabel={'Picture of laddle inside a bowl with meat'}
+      />
+      <Image
+        style={welcomeStyles.image} 
+        source={require('../img/Picture3.png')}
+        resizeMode='cover'
+        accessibility={true}
+        accessibilityLabel={'Picture of orange been cut into two'}
+      />
+      <Image
+        style={welcomeStyles.image} 
+        source={require('../img/Picture4.png')}
+        resizeMode='cover'
+        accessibility={true}
+        accessibilityLabel={'Picture of fishes'}
+      />
+    </ScrollView>
   );
 }
 
@@ -16,7 +50,6 @@ const welcomeStyles = StyleSheet.create({
   logo: {
     height: 100,
     width: 300,
-    resizeMode: 'contain',
   },
   container: {
     flex: 1,
@@ -32,6 +65,11 @@ const welcomeStyles = StyleSheet.create({
     fontSize: 20,
     fontWeight: 'bold'
   },
+  image: {
+    width: 350,
+    height: 250,
+    borderRadius: 10,
+  }
 })
 
 export default Welcome;
