6466a1d6bea4f4c625cc07af9d3d4485f76a36d5
Sakyi-Ken <esappiah009@st.ug.edu.gh>
2024-10-09 21:21:49 +0000

    Add a folder to handle the navigation of the screens.

---
+++ navigators/Features.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+
+import Services from './Services';
+import Feedback from '../screens/FeedbackForm';
+
+const Drawer = createDrawerNavigator();
+
+export default function Features() {
+  return (
+  <Drawer.Navigator
+    screenOptions={{
+      drawerPosition: 'right',
+      drawerType: 'slide',
+      drawerStyle: {
+        backgroundColor: '#FBDABB',
+        width: 200,
+      },
+      drawerActiveTintColor: '#fff',
+      drawerInactiveTintColor: '#333333',
+      drawerActiveBackgroundColor: '#333333',
+      drawerInactiveBackgroundColor: '#FBDABB',
+    }}
+  >
+    <Drawer.Screen name="Services" component={Services} />
+    <Drawer.Screen name="Feedback" component={Feedback} />
+  </Drawer.Navigator>
+  );
+}
