2ee6bf39d940669d03363bb842fcfd39dab3506e
Sakyi-Ken <esappiah009@st.ug.edu.gh>
2024-10-11 01:52:54 +0000

    Change the styles in both screens.

--- screens/SubscribeScreen.js
+++ screens/SubscribeScreen.js
@@ -1,9 +1,80 @@
-import * as React from 'react';
-import { View } from 'react-native';
+import * as React from "react";
+import { 
+  View, 
+  Image, 
+  TextInput, 
+  Text, 
+  StyleSheet, 
+  Alert,
+  KeyboardAvoidingView 
+} from 'react-native';
+import Button from '../components/Button.js';
+import {validateEmail} from '../utils';
 
 const SubscribeScreen = () => {
-  // Add subscribe screen code here
-  return <View></View>;
+  const [email, setEmail] = React.useState('')
+
+  const isEmailValid = validateEmail(email);
+
+  return (
+      <View style={subscribeStyles.container}>
+        <View style={subscribeStyles.contentContainer}>
+          <Image 
+            style={subscribeStyles.Image} 
+            source={require('../img/little-lemon-logo-grey.png')}
+            resizeMode='contain'
+            accessible={true}
+            accessibilityLabel={'Little Lemon Logo'}
+          />
+          <Text style={subscribeStyles.text}>Subscribe to our newsletter for our latest delicious recipes!</Text>
+          </View>
+        <TextInput
+          style={subscribeStyles.input}
+          value={email}
+          onChangeText={setEmail}
+          placeholder={'Type your email'}
+          keyboardType='email-address'
+          //textContentType='emailAddress'
+        />
+        <Button 
+          onPress={() => 
+            Alert.alert('Thanks for subscribing, stay tuned!'
+          )}
+          disabled={!isEmailValid}
+        >
+          Subscribe
+        </Button>
+      </View>
+  );
 };
+
+const subscribeStyles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    padding: 24,
+    justifyContent: 'center' 
+  },
+  Image: {
+    width: 350,
+    height: 100,
+    marginBottom: 32,
+    marginTop: 30,
+  },
+  text: {
+    color: '#333333',
+    fontSize: 20,
+    textAlign: 'center',
+  },
+  input: {
+    height: 40,
+    marginVertical: 24,
+    borderWidth: 1,
+    padding: 10,
+    borderRadius: 8,
+    fontSize: 16,
+    borderColor: 'EDEFEE',
+  },
+});
 
 export default SubscribeScreen;
--- screens/Welcome.js
+++ screens/Welcome.js
@@ -1,28 +1,30 @@
-import React from 'react';
-import { Image, Text, StyleSheet, View, Pressable } from 'react-native';
+import * as React from 'react';
+import { Image, Text, StyleSheet, View } from 'react-native';
+import Button from '../components/Button.js';
 
 const Welcome = ( {navigation} ) => {
   return (
     <View style={welcomeStyles.container}>
-      <Image 
-        style={welcomeStyles.Image} 
-        source={require('../img/little-lemon-logo (2).png')}
-        resizeMode="center"
-        accessible={true}
-        accessibilityLabel={'Little Lemon Logo'} 
-      />
-      <View style={welcomeStyles.containerTitle}>
+      <View style={welcomeStyles.contentContainer}>
+        <Image 
+          style={welcomeStyles.Image} 
+          source={require('../img/little-lemon-logo (2).png')}
+          resizeMode="contain"
+          accessible={true}
+          accessibilityLabel={'Little Lemon Logo'} 
+        />
         <Text style={welcomeStyles.title}>
-          Little Lemon, your local
-        </Text>
-        <Text style={welcomeStyles.title}>
-          Mediterranean Bistro
+          Little Lemon, your local Mediterranean Bistro
         </Text>
       </View>
   
-      <Pressable style={welcomeStyles.button}>
-        <Text style={welcomeStyles.buttonText}>Newsletter</Text>
-      </Pressable>
+      <Button
+        onPress={() => {
+          navigation.navigate('Subscribe');
+        }}
+      >
+        Newsletter
+      </Button>
     </View>
   );
 }
@@ -30,35 +32,27 @@ const Welcome = ( {navigation} ) => {
 const welcomeStyles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'flex-end',
-    alignItems: 'center',
+    justifyContent: 'space-between',
     backgroundColor: '#fff',
+    padding: 24,
+  },
+  contentContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
   },
   Image: {
     width: 200,
-    height: 200,
+    height: 300,
   },
   title: {
-    fontSize: 22,
+    fontSize: 20,
     color: '#333333',
-  },
-  containerTitle: {
-    alignItems: 'center',
-    marginTop: 70,
-    marginBottom: 80,
-  },
-  button:{
-    backgroundColor: '#495E57',
-    padding: 10,
-    borderRadius: 8,
-    marginTop: 70,
-    width: 350,
-    marginBottom: 50,
-  },
-  buttonText:{
-    color: '#EDEFEE',
-    fontSize: 18,
     textAlign: 'center',
+    marginTop: 48,
+    fontWeight: 'bold',
+    paddingVertical: 10,
+
   },
 });
 export default Welcome;
