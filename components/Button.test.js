819ab3e24e13164dba95629b1aff9b1021e9055b
Sakyi-Ken <esappiah009@st.ug.edu.gh>
2024-10-16 17:33:18 +0000

    Remove Jest configurations

--- components/Button.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import Button from '../components/Button';
-
-test('renders button component', () => {
-  render(<Button />);
-  const buttonElement = screen.getByText(/Button/i);
-  expect(buttonElement).toBeInTheDocument();
-});
