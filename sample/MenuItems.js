43c88979d3c3cc154b0df62e680b051773db6bf8
Sakyi-Ken <esappiah009@st.ug.edu.gh>
2024-10-23 05:03:09 +0000

    Correct the spelling of flexDirection.

--- sample/MenuItems.js
+++ sample/MenuItems.js
@@ -65,7 +65,7 @@ const menuStyles = StyleSheet.create({
     paddingHorizontal: 40,
     paddingVertical: 20,
     backgroundColor:  '#495E57',
-    flowDirection: 'row',
+    flexDirection: 'row',
     justifyContent: 'space-between', 
   },
   itemText: {
